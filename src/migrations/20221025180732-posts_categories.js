a1601538c78aaf4c532c3d362fc749137124513e
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-10-25 15:27:43 -0300

    cria as migrations paras as tabelas users, categories, blog_ports e posts_categories

---
+++ src/migrations/20221025180732-posts_categories.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  },
+};
