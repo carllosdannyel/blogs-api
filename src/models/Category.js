9383c7de9e3b5aedcbd0d9c72c21b3ab372dfe77
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-10-25 22:42:55 -0300

    cria a model Category

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      tableName: 'categories',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+
+  return Category;
+};
