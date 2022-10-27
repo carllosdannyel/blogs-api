5b22f3ce9ecec77fee7303b6b6eef996487d1c45
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-10-27 18:15:39 -0300

    refatora schemas joi

--- src/schemas/login.validation.js
+++ src/schemas/login.validation.js
@@ -1,4 +1,15 @@
-const { loginSchema } = require('./schemas');
+const Joi = require('joi');
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'any.required': 'Some required fields are missing',
+    'string.empty': 'Some required fields are missing',
+  }),
+  password: Joi.string().required().messages({
+    'any.required': 'Some required fields are missing',
+    'string.empty': 'Some required fields are missing',
+  }),
+});
 
 const loginValidation = (body) => {
   const { error } = loginSchema.validate(body);
--- src/schemas/user.validation.js
+++ src/schemas/user.validation.js
@@ -1,4 +1,23 @@
-const { userSchema } = require('./schemas');
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+  displayName: Joi.string().min(8).required().messages({
+    'any.required': '"displayName" is required',
+    'string.empty': '"displayName" is required',
+    'strinig.min': '"displayName" length must be at least 8 characters long',
+  }),
+  email: Joi.string().email().required().messages({
+    'any.required': '"email" is required',
+    'string.empty': '"email" is required',
+    'string.email': '"email" must be a valid email',
+  }),
+  password: Joi.string().min(6).required().messages({
+    'any.required': '"password" is required',
+    'string.empty': '"password" is required',
+    'string.min': '"password" length must be at least 6 characters long',
+  }),
+  image: Joi.string(),
+});
 
 const userValidation = (body) => {
   const { error } = userSchema.validate(body);
